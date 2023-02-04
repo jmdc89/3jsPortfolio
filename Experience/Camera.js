d05cbd57cddc3fda107ae0d52b7102a71d284a3a
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-04 14:36:48 +0300

    Update Camera.js
    perspective and ortographic cameras

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -1,3 +1,4 @@
+import * as THREE from "three";
 import Experience from "./Experience.js";
 
 export default class Camera {
@@ -6,6 +7,32 @@ export default class Camera {
         this.sizes = this.experience.sizes;
         this.scene = this.experience.scene;
         this.canvas = this.experience.canvas;
-        console.log(this.experience, this.sizes, this.scene, this.canvas);
+
+        this.createPerspectiveCamera();
+        this.createOrthographicCamera();
+        
+    }
+
+    createPerspectiveCamera() {
+        this.perspectiveCamera = new THREE.PerspectiveCamera(
+            35,
+            this.sizes.aspect,
+            0.1,
+            1000
+        );
+        this.scene.add(this.perspectiveCamera);
+    }
+
+    createOrthographicCamera() {
+        this.orthographicCamera = new THREE.OrthographicCamera(
+            (-this.sizes.aspect * this.sizes.frustrum) / 2,
+            (this.sizes.aspect * this.sizes.frustrum) / 2,
+            this.sizes.frustrum / 2,
+            -this.sizes.frustrum / 2,
+            -50,
+            50
+        );
+
+        this.scene.add(this.orthographicCamera);
     }
 }
