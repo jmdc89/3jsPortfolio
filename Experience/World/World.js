7a5bdbb42314205223e99dde4140eaecb46f8df0
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-04 17:12:22 +0300

    Create World.js
    World class

---
+++ Experience/World/World.js
@@ -0,0 +1,10 @@
+import * as THREE from "three";
+import Experience from "../Experience.js";
+
+import {EventEmitter} from "events";
+
+export default class World extends EventEmitter {
+    constructor() {
+        
+    }
+}
