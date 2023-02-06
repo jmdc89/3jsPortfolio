3f7d76cf5c0a4b69531c028b091918b1dc82ba3c
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-06 08:54:44 +0300

    Update Room.js
    Shadows

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -15,9 +15,15 @@ export default class Room {
     }
 
     setModel() {
+
+        this.actualRoom.children.forEach(child => {
+            child.castShadow = true;
+            child.receiveShadow = true;
+        });
+
         this.scene.add(this.actualRoom);
         this.actualRoom.scale.set(0.11,0.11,0.11);
-        this.actualRoom.rotation.y += Math.PI / 4;
+        // this.actualRoom.rotation.y += Math.PI / 4;
     }
 
     resize() {}
