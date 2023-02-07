47342633d838804d64f1e0e14edddf07c9694982
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-07 13:48:30 +0300

    Update Controls.js
    CatmullRomCurve

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -13,7 +13,25 @@ export default class Controls {
         this.setPath();
     }
 
-    setPath() {}
+    setPath() {
+
+        this.curve = new THREE.CatmullRomCurve3( [
+            new THREE.Vector3( -10, 0, 10 ),
+            new THREE.Vector3( -5, 5, 5 ),
+            new THREE.Vector3( 0, 0, 0 ),
+            new THREE.Vector3( 5, -5, 5 ),
+            new THREE.Vector3( 10, 0, 10 )
+        ] );
+
+        const points = this.curve.getPoints( 50 );
+        const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+        const material = new THREE.LineBasicMaterial( { color: 0xff0000 } );
+
+        // Create the final object to add to the scene
+        const curveObject = new THREE.Line( geometry, material ); 
+        this.scene.add(curveObject);
+        }
 
     resize() {}
 
