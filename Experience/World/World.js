672535e9a2ee65a3f5c23b15cf8ada0ee43428b6
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-07 13:31:06 +0300

    Update World.js
    controls import and update

--- Experience/World/World.js
+++ Experience/World/World.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import Experience from "../Experience.js";
 
 import Room from "./Room.js";
+import Controls from "./Controls.js";
 import Environment from "./Environment.js";
 import {EventEmitter} from "events";
 
@@ -18,6 +19,7 @@ export default class World extends EventEmitter {
         this.resources.on("ready", () => {
             this.environment = new Environment();
             this.room = new Room();
+            this.controls = new Controls();
         });
 
     }
@@ -28,6 +30,9 @@ export default class World extends EventEmitter {
     update() {
         if(this.room) {
             this.room.update();
+        }
+        if(this.controls) {
+            this.controls.update();
         }
     }
 }
