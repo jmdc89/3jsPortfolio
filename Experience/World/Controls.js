e1cbaaa1b97ad5fe9274e62fba0be032e6eb0d41
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-07 12:31:46 +0300

    Create Controls.js
    constructor

---
+++ Experience/World/Controls.js
@@ -0,0 +1,21 @@
+import * as THREE from "three";
+import Experience from "../Experience.js";
+
+export default class Controls {
+    constructor() {
+        this.experience = new Experience();
+        this.scene = this.experience.scene;
+        this.sizes = this.experience.sizes;
+        this.resources = this.experience.resources;
+        this.time = this.experience.time;
+        this.camera = this.experience.camera;
+
+        this.setPath();
+    }
+
+    setPath() {}
+
+    resize() {}
+
+    update() {}
+}
