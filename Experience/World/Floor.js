94ba868e4d1f20a8dc8f85b726b2185f73bd0aea
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-09 16:06:01 +0300

    Update Floor.js
    parameters

--- Experience/World/Floor.js
+++ Experience/World/Floor.js
@@ -3,6 +3,16 @@ import Experience from "../Experience.js";
 
 export default class Floor {
     constructor() {
-        
+
+        this.experience = new Experience();
+        this.scene = this.experience.scene;
+
+        this.setFloor();
+        this.setCircles();
+
     }
+
+    resize() {}
+
+    update() {}
 }
