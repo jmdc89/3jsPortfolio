5420b001287db6da305b0585f7fd2f94d3717dd6
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-12 09:53:07 +0300

    Update Controls.js
    setPath

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,5 +1,8 @@
 import * as THREE from "three";
 import Experience from "../Experience.js";
+import GSAP from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+// import ASScroll from "@ashthornton/asscroll";
 
 
 export default class Controls {
@@ -10,7 +13,29 @@ export default class Controls {
         this.resources = this.experience.resources;
         this.time = this.experience.time;
         this.camera = this.experience.camera;
+        this.room = this.experience.world.room.actualRoom;
+        GSAP.registerPlugin(ScrollTrigger);
 
+        this.setPath();
+
+    }
+
+    setPath() {
+        console.log(this.room);
+        this.timeline = new GSAP.timeline();
+        this.timeline.to(this.room.position, {
+            x: () => {
+                return this.sizes.width * 0.0012;
+            },
+            scrollTrigger: {
+                trigger: ".first-move",
+                markers: true,
+                start: "top top",
+                end: "bottom bottom",
+                scrub: 0.6,
+                invalidateOnRefresh: true,
+            },
+        });
     }
 
     resize() {}
