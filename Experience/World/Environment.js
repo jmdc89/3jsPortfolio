457f274fd08f41b79602f92ff3d1c191a635037f
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-12 11:03:25 +0300

    Update Environment.js
    setGUI

--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -9,9 +9,26 @@ export default class Environment {
         this.scene = this.experience.scene;
         this.resources = this.experience.resources;
 
-        const gui = new GUI();
+        this.gui = new GUI();
+        this.obj = {
+            colorObj: { r: 0, g: 0, b: 0 },
+            intensity: 3,
+        };
 
         this.setSunlight();
+        this.setGUI();
+    }
+
+    setGUI() {
+        this.gui.addColor(this.obj, "colorObj").onChange(() => {
+            this.sunLight.color.copy(this.obj.colorObj);
+            this.ambientLight.color.copy(this.obj.colorObj);
+            console.log(this.obj.colorObj);
+        });
+        this.gui.add(this.obj, "intensity", 0, 10).onChange(() => {
+            this.sunLight.intensity = this.obj.intensity;
+            this.sunLight.ambientLight = this.obj.intensity;
+        });
     }
 
     setSunlight() {
