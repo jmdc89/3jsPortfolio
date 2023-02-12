fed9f4f19eb1ef7bb083a471a37fba5406cd9b9b
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-12 11:00:50 +0300

    lilGui
    npm lil gui

--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -1,11 +1,15 @@
 import * as THREE from "three";
 import Experience from "../Experience.js";
 import GSAP from "gsap";
+import GUI from "lil-gui";
 
 export default class Environment {
     constructor() {
         this.experience = new Experience();
         this.scene = this.experience.scene;
+        this.resources = this.experience.resources;
+
+        const gui = new GUI();
 
         this.setSunlight();
     }
